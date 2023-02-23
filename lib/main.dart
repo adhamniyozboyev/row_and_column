bb83f21c0c12c2755578cf2a1d5c57872d395bb5
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-23 12:07:26 +0500

    print my name

--- lib/main.dart
+++ lib/main.dart
@@ -2,70 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MyWidget(),
+    home: MyApp(),
   ));
 }
 
-class MyWidget extends StatelessWidget {
-  Widget func(double size) {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget func(double size, String text, Color color) {
     return Container(
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.blue,
-      ),
+      color: color,
       height: size,
       width: size,
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
     );
   }
 
-  const MyWidget({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              func(120),
-              func(120),
-              func(120),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              func(120),
-              func(120),
-              func(120),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              func(120),
-              func(120),
-              func(120),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              func(120),
-              func(120),
-              func(120),
-            ],
-          ),
-                 Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              func(120),
-              func(120),
-              func(120),
-            ],
-          ),
-        ],
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            func(80, 'A', Colors.red),
+            func(80, 'D', Colors.blue),
+            func(80, 'H', Colors.green),
+            func(80, 'A', Colors.pink),
+            func(80, 'M', Colors.orange),
+          ],
+        ),
       ),
     );
   }
