1b0a6d20ff10a45fbb1db4ed1dc2a9b8a320fb06
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-23 08:56:00 +0500

    first path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,69 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(MaterialApp());
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                height: 120,
+                width: 120,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                height: 120,
+                width: 120,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                height: 120,
+                width: 120,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                height: 120,
+                width: 120,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                height: 120,
+                width: 120,
+              ),
+            ],
+          )
+        ],
+      ),
+    ),
+  ));
 }
